531c99b0b745d3adf55b566381d7e8b630af7d21
elvxk <pradiptaramadan9c@gmail.com>
2024-11-14 02:31:09 +0700

    update(config): role name

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const year = new Date().getFullYear();
 
 export const bot_name = "SEMVR";
-export const environment = "development"; // development or production
+export const environment = "production"; // development or production
 export const footer = `Copyright © ${year} SEMVR | Made with love ✨`;
-export const deploy_commands = false;
+export const deploy_commands = true;
 export const send_announce = false;
-export const role_name = "SEMVR V1";
+export const role_name = "SEMVR";
